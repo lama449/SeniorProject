d190f2dcf802545849951904183b5c20dfd0b82e
cthatchr <36424594+cthatchr@users.noreply.github.com>
2019-10-10 15:34:08 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask
-from flask_restful import Api
 from flask_pymongo import PyMongo
-from resources.facility import *
-from resources.building import *
-from resources.room import *
-from resources.reservation import *
-from resources.maintenance import *
-from resources.user import *
-from resources.group import *
+from flask_restful import *
+import bcrypt
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/managementdb"
-mongo = PyMongo(app)
+def getDB():
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/managementdb"
+    mongo = PyMongo(app)
+    return mongo
 
+@app.route('/', '/home')
+def home():
+    return 'This is a page. I made this :)'
 
 @app.route('/login', methods = ['POST'])
 def login():
     # login
 
+from resources.reservation import Reservation
 api.add_resource(Reservation, '/reservations', '/reservations/<str:id>')
--- resources/reservation.py
+++ resources/reservation.py
@@ -1,7 +1,11 @@
 from flask_restful import Resource
+from app import getDB
+
+db = getDB()
+
 
 class Reservation(Resource):
-    def get(self):
+    def get(self):  # returns the reservation information
         pass
 
     def post(self):
