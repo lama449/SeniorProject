d4c79de92cd90e77e6d5379855133d98961709af
Eric Zielonka <zielonka.eric@gmail.com>
2019-10-15 19:43:08 +0000

    moved user registration

--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, render_template, redirect, url_for, session
 from flask_restful import Resource, request
 import bcrypt
 from SeniorProject import database
@@ -11,17 +12,31 @@ class User(Resource):
         print(users)
 
     def post(self):  # creating a new user
-        data = request.json
-
+        # load template, then pass info to User resource to POST new user
+        # session['username'] = request.json['username']  # session retruned user
+        data = request.form
         users = db.users
-        existing_user = users.find_one({'username': data.get('username')})  # try to find username in the db
-
-        if existing_user is None:  # create user
-            hashpass = bcrypt.hashpw(data.get('password').encode('utf-8'), bcrypt.gensalt())
-            users.insert({'username': data.get('username'), 'password': hashpass})
-            return {'username': data.get('username')}
+        existing_user = users.find_one({'username': data.get('username')})
+        if existing_user is None:
+            email = data.get('email')
+            existing_email = users.find_one({'email': email})
+            if '@' not in email or existing_email:
+                return 'That email already exists.'
+    
+            hashed_password = bcrypt.hashpw(data.get('password').encode('utf-8'), bcrypt.gensalt())
+            users.insert({
+                'username': data.get('username'),
+                'password': hashed_password,
+                'email': email,
+                'first_name': '',
+                'last_name': '',
+                'groupID': []
+            })
+            session['username'] = data.get('username')
+            return redirect(url_for('calendar'))
         else:
-            return {'message': 'username already exists'}
+            return 'That username already exists.'
+
 
     def put(self):
         pass
