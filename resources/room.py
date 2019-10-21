0d3e3011bcbdca27e7882855df966d2b17502c9e
cthatchr <thatch72@students.rowan.edu>
2019-10-21 21:00:31 +0000

    fixed issue with room.py

--- resources/room.py
+++ resources/room.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from flask_restful import Resource, request
 import bcrypt
+from bson.json_util import dumps
 from SeniorProject import database
 
 db = database.conn_DB()
@@ -8,8 +9,6 @@ db = database.conn_DB()
 class Room(Resource):
     def get(self, f_id, b_id, r_id=None): # return a list of rooms for building
         rooms = db.rooms
-        if r_id is None:
-            return {'message': 'room list'}
         facilities = db.facilities
         buildings = db.buildings
         current_facility = facilities.find_one({'name': f_id})
@@ -17,8 +16,15 @@ class Room(Resource):
             current_building = buildings.find_one({'name': b_id, 'facilityID': current_facility.get('_id')})
                                                
             if current_building:  # if building exists
-                current_rooms = rooms.find({'buildingID': current_building.get('_id')})
-                return dumps(current_rooms)
+                if r_id is None:  # return list of rooms
+                    current_rooms = rooms.find({'buildingID': current_building.get('_id')})
+                    return dumps(current_rooms)
+                else:  # specific room
+                    current_room = rooms.find_one({'number': int(r_id), 'buildingID': current_building.get('_id')})
+                    if current_room:
+                        return dumps(current_room)
+                    else:
+                        return 'Invalid room'
             else:
                 return 'Invalid building'
         else:
