b93e88a024e40280251c040643233a3e37fcac22
lama449 <47458231+lama449@users.noreply.github.com>
2019-11-07 20:19:21 +0000

    Added the creation for facility.

--- resources/facility.py
+++ resources/facility.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template, redirect, url_for, session, j
 from flask_restful import Resource, request
 from bson.objectid import ObjectId
 import bcrypt
+import string
+import random
 from SeniorProject import database
 
 db = database.conn_DB()
@@ -16,7 +18,36 @@ class Facility(Resource):
             return 'Invalid facility'
 
     def post(self):
-        pass
+        facilities = db.facilities
+        data = request.form
+        while data.get('private') == 'true':
+            access_code = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+            check_facility = facilities.find_one({'access_code': access_code})
+            if check_facility is None:
+                break
+        takeID = facilities.insert_one({
+        'name': data.get('name'),
+        'private': data.get('private') == 'true',
+        'access_code': access_code if (data.get('private') == 'true') else '',              
+        'address': [{
+            'address_L1': data.get('address_L1'),
+            'address_L2': data.get('address_L2'),
+            'city': data.get('city'),
+            'state': data.get('state'),
+            'zip': data.get('zip'),
+            'country': data.get('country')
+            }],
+        'phone': data.get('phone'),
+        'description': data.get('description'),
+        'presets': {},
+        'attributes': {},
+        'maintenance': {},
+        'groups': [{
+            '_id': ObjectId(),
+            'name': 'admin'
+            }]
+        })
+        return jsonify({'_id': takeID.inserted_id})
 
     def put(self):
         pass
