d0a71405152a45598fb6bae358650b376bf0775a
EC2 Default User <ec2-user@ip-172-31-83-61.ec2.internal>
2019-11-10 04:16:16 +0000

    Revert "changed how id's are passed and queires are handled"
    This reverts commit fb220e3bde39d8cf7c65804a5f37a4c65dc686f1.

--- resources/room.py
+++ resources/room.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, session, jsonify
 from flask_restful import Resource, request
 import bcrypt
-from bson.objectid import ObjectId
 #from bson.json_util import dumps
 from SeniorProject import database
 
@@ -12,9 +11,9 @@ class Room(Resource):
         rooms = db.rooms
         facilities = db.facilities
         buildings = db.buildings
-        current_facility = facilities.find_one({'_id': ObjectId(f_id)})
+        current_facility = facilities.find_one({'name': f_id})
         if current_facility:
-            current_building = buildings.find_one({'_id': ObjectId(b_id), 'facilityID': current_facility.get('_id')})
+            current_building = buildings.find_one({'name': b_id, 'facilityID': current_facility.get('_id')})
                                                
             if current_building:  # if building exists
                 if r_id is None:  # return list of rooms
@@ -25,7 +24,7 @@ class Room(Resource):
                     return jsonify(current_rooms)
                     #return dumps(current_rooms)
                 else:  # specific room
-                    current_room = rooms.find_one({'_id': ObjectId(r_id), 'buildingID': current_building.get('_id')})
+                    current_room = rooms.find_one({'number': int(r_id), 'buildingID': current_building.get('_id')})
                     if current_room:
                         return jsonify(current_room)
                         #return dumps(current_room)
@@ -50,9 +49,9 @@ class Room(Resource):
         facilities = db.facilities
         buildings = db.buildings
         
-        current_facility = facilities.find_one({'_id': ObjectId(f_id)})
+        current_facility = facilities.find_one({'name': f_id})
         if current_facility:
-            current_building = buildings.find_one({'_id': ObjectId(b_id), 'facilityID': current_facility.get('_id')})                                 
+            current_building = buildings.find_one({'name': b_id, 'facilityID': current_facility.get('_id')})                                 
             if current_building:  # if building exists
                 takeID = rooms.insert_one({
                 'attributes': {},
