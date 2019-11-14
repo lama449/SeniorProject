176ec8fc173fef2927dde3cdd2f5199a5d8b48b3
Eric Zielonka <zielonka.eric@gmail.com>
2019-11-14 01:09:33 -0500

    Adds homepage frontend, adds to css, endpoints
     - Adds the frontend for the homepage
       - search boxes to search for facilities
       - uses Axios and Vue to call endpoint and display results
         - generates table dynamically to show list of facilities with
           appropriate links
     - Changes CSS
       - adds the styles for the registration page into homestyles.css
       - adds styles for the form and table (possibly temporary, might be
         changed soon)
       - adds some basic styles for font-size
       - removes the borders between the buttons on the nav bar
       - organizes homestyles.css more
       - changes the indentation to be consistent (all spaces)
     - Adds 'endpoint' parameter to each resource in app.py

--- app.py
+++ app.py
@@ -71,12 +71,11 @@ def building_confirmation():
 
 @app.route('/building_creation', methods=['GET'])
 def building_creation():
-    return render_template('Building_Creation_Page.html')  
-    
-api.add_resource(Facility, '/facilities', '/facilities/<f_id>')
-api.add_resource(Building, '/facilities/<f_id>/buildings', '/facilities/<f_id>/buildings/<b_id>')
-api.add_resource(Room, '/facilities/<f_id>/buildings/<b_id>/rooms', '/facilities/<f_id>/buildings/<b_id>/rooms/<r_id>')
-# api.add_resource(Room, '/rooms', '/rooms/<r_id>')
+    return render_template('Building_Creation_Page.html')
+
+api.add_resource(Facility, '/facilities', '/facilities/<f_id>', endpoint='facility')
+api.add_resource(Building, '/facilities/<f_id>/buildings', '/facilities/<f_id>/buildings/<b_id>', endpoint='building')
+api.add_resource(Room, '/facilities/<f_id>/buildings/<b_id>/rooms', '/facilities/<f_id>/buildings/<b_id>/rooms/<r_id>', endpoint='room')
 api.add_resource(Reservation, '/reservations', endpoint='reservation')
 api.add_resource(User, '/users', '/users/<u_id>', endpoint='user')
 
