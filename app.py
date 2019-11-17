a19439cad79722f08ec44777ff460701ec02a937
Monica Mahon <monicamahon0@gmail.com>
2019-11-17 19:05:46 +0000

    Added maintenance endpoint to app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from SeniorProject.resources.user import User
 from SeniorProject.resources.facility import Facility
 from SeniorProject.resources.room import Room
 from SeniorProject.resources.building import Building
+from SeniorProject.resources.maintenance import Maintenance
 from SeniorProject.new_json_encoder import New_JSON_Encoder
 
 app = Flask(__name__)
@@ -70,6 +71,7 @@ api.add_resource(Building, '/facilities/<f_id>/buildings', '/facilities/<f_id>/b
 api.add_resource(Room, '/facilities/<f_id>/buildings/<b_id>/rooms', '/facilities/<f_id>/buildings/<b_id>/rooms/<r_id>', endpoint='room')
 api.add_resource(Reservation, '/reservations', endpoint='reservation')
 api.add_resource(User, '/users', '/users/<u_id>', endpoint='user')
+api.add_resource(Maintenance, '/facilities/<f_id>', '/facilities/<f_id>/rooms/<r_id>', endpoint='maintenance')
 
 if __name__ == '__main__':
     app.run(debug=True)
