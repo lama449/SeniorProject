56ba799fe1a3da572f6c70c9ec1cb338b7fe44c4
Monica Mahon <monicamahon0@gmail.com>
2019-11-17 21:43:56 +0000

    Fixed url for maintenance endpoint

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ api.add_resource(Building, '/facilities/<f_id>/buildings', '/facilities/<f_id>/b
 api.add_resource(Room, '/facilities/<f_id>/buildings/<b_id>/rooms', '/facilities/<f_id>/buildings/<b_id>/rooms/<r_id>', endpoint='room')
 api.add_resource(Reservation, '/reservations', endpoint='reservation')
 api.add_resource(User, '/users', '/users/<u_id>', endpoint='user')
-api.add_resource(Maintenance, '/facilities/<f_id>', '/facilities/<f_id>/rooms/<r_id>', endpoint='maintenance')
+api.add_resource(Maintenance, '/facilities/<f_id>/maintenance', '/facilities/<f_id>/maintenance/<r_id>', endpoint='maintenance')
 
 if __name__ == '__main__':
     app.run(debug=True)
