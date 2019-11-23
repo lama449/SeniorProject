da98c5d2d366e72ba755c158cb75630283243c5f
Eric Zielonka <zielonka.eric@gmail.com>
2019-11-23 18:11:05 +0000

    adds user PUT

--- resources/user.py
+++ resources/user.py
@@ -24,9 +24,7 @@ class User(Resource):
             return jsonify(res)
 
     def post(self):  # creating a new user
-        # load template, then pass info to User resource to POST new user
-        # session['username'] = request.json['username']  # session retruned user
-        data = request.form
+        data = request.json
         users = db.users
         #response object
         res = {
@@ -40,6 +38,9 @@ class User(Resource):
         password_confirm = data.get('password2')
         f_name = data.get('fname')
         l_name = data.get('lname')
+        question = data.get('question')
+        answer = data.get('answer')
+        
 
         if '@' not in email:
             res['err'].append('Invalid email.')
@@ -48,9 +49,13 @@ class User(Resource):
         if password != password_confirm:
             res['err'].append('Passwords do not match.')
         if not f_name:
-            res['err'].append('No First Name')
+            res['err'].append('No first name')
         if not l_name:
-            res['err'].append('No Last Name')
+            res['err'].append('No last name')
+        if not question:
+            res['err'].append('No security question')
+        if not answer:
+            res['err'].append('No security question answer')
 
         if res['err']:
             return jsonify(res)
@@ -62,11 +67,15 @@ class User(Resource):
             return jsonify(res)
 
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hashed_answer = bcrypt.hashpw(answer.encode('utf-8'), bcrypt.gensalt())
+
         users.insert({
             'password': hashed_password,
             'email': email,
             'first_name': f_name,
             'last_name': l_name,
+            'question': question,
+            'answer': hashed_answer,
             'groupID': []
         })
         res['msg'].append('success')
@@ -74,7 +83,98 @@ class User(Resource):
 
 
     def put(self):
-        pass
+        data = request.json
+        users = db.users
+        #response object
+        res = {
+            'msg': [],
+            'err': []
+        }
+
+        if data.get('email'):
+            # check if all the fields are filled out
+            email = data.get('email')
+            f_name = data.get('first_name')
+            l_name = data.get('last_name')
+            question = data.get('question')
+            answer = data.get('answer')
+
+            if '@' not in email:
+                res['err'].append('Invalid email.')
+            if not f_name:
+                res['err'].append('No First Name')
+            if not l_name:
+                res['err'].append('No Last Name')
+            if not question:
+                res['err'].append('No security question')
+            if not answer:
+                res['err'].append('No security question answer')
+
+            if res['err']:
+                return jsonify(res)
+
+            if email == session.get('user').get('email'):
+                # email is the same as it was before, so it's good
+                pass
+            elif users.find_one({'email': email}):
+                # email is associated with another user
+                res['err'].append('That email already exists.')
+                return jsonify(res)
+    
+            hashed_answer = bcrypt.hashpw(answer.encode('utf-8'), bcrypt.gensalt())
+
+            users.update_one({'_id': session.get('user').get('_id')}, {
+                '$set': {
+                    'email': email,
+                    'first_name': f_name,
+                    'last_name': l_name,
+                }
+            })
+            res['msg'].append('success')
+            return jsonify(res)
+        elif data.get('password'):
+            # check if all the fields are filled out
+            old_password = data.get('opsw')
+            new_password = data.get('npsw')
+            confirm_password = data.get('cpsw')
+
+            if not old_password:
+                res['err'].append('No old password')
+            if not new_password:
+                res['err'].append('No new password')
+            if not confirm_password:
+                res['err'].append('No password confirmation')
+            if new_password != confirm_password:
+                res['err'].append('Passwords do not match')
+            if new_password == old_password:
+                res['err'].append('New password is the same as old password')
+            if not f_name:
+                res['err'].append('No First Name')
+            if not l_name:
+                res['err'].append('No Last Name')
+
+            if res['err']:
+                return jsonify(res)
+
+            hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+
+            users.update_one({'_id': session.get('user').get('_id')}, {
+                '$set': {
+                    'password': hashed_password,
+                }
+            })
+            res['msg'].append('success')
+            return jsonify(res)
+        else:
+            res['err'].append('Invalid arguments')
+            return jsonify(res)
+
 
     def delete(self):
         pass
+
+
+
+
+
+
