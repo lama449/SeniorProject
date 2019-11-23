93ac865448a1fc8a95e57565e472e1ebd032bfef
Monica Mahon <monicamahon0@gmail.com>
2019-11-23 19:50:04 +0000

    GET groups endpoint

--- resources/group.py
+++ resources/group.py
@@ -1,8 +1,37 @@
-from flask_restful import Resource
+# groups.py
+# Endpoints for managing maintenance requests
+# Monica Mahon, 11/23/2019
+
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify
+from flask_restful import Resource, request
+import bcrypt
+from SeniorProject import database
+from bson.objectid import ObjectId
+
+db = database.conn_DB()
 
 class Group(Resource):
-    def get(self):
-        pass
+    def get(self, f_id, g_id=None):
+        facilities = db.facilities
+        current_facility = facilities.find_one({'_id': ObjectId(f_id)})
+        if current_facility:  # if facility exists
+            if g_id is None:
+                return jsonify(facilities.find_one({'_id': ObjectId(f_id)},
+                                                   {'_id': 0, 'access_code': 0, 'name': 0, 'private': 0, 'address': 0,
+                                                    'phone': 0, 'description': 0, 'presets': 0, 'attributes': 0,
+                                                    'maintenance': 0}))
+            else:
+                groups = facilities.find(
+                    {'_id': ObjectId(f_id), 'groups': {'$elemMatch': {'_id': ObjectId(g_id)}}},
+                    {'_id': 0, 'access_code': 0, 'name': 0, 'private': 0, 'address': 0,
+                     'phone': 0, 'description': 0, 'presets': 0, 'attributes': 0, 'maintenance': 0})
+                current_group = [group for group in groups]
+                if current_group:
+                    return jsonify(current_group)
+                else: 
+                    return 'No groups to display'
+        else: 
+            return 'Invalid facility ID'
 
     def post(self):
         pass
