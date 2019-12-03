c6c411f8bea854d2149752cd2b588176fbe89acc
Eric Zielonka <zielonka.eric@gmail.com>
2019-12-03 04:53:59 +0000

    adds functionality to edit public/private in a facility

--- resources/facility.py
+++ resources/facility.py
@@ -121,14 +121,36 @@ class Facility(Resource):
             'msg': [],
             'err': []
         }
+
         current_facility = facilities.find_one({'_id': ObjectId(f_id)})
+
         if not check_admin(f_id):
             res['err'].append('You do not have the permissions to edit this facility.')
             return jsonify(res)
         if current_facility:
+            access_code = ""
+
+            if current_facility.get('private'):
+                if data.get('private'):
+                    access_code = current_facility.get('access_code')
+                else:
+                    access_code = ""
+            else:
+                if data.get('private'):
+                    while True:
+                        access_code = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+                        check_facility = facilities.find_one({'access_code': access_code})
+                        if check_facility is None:
+                            break
+                else:
+                    access_code = ""
+            print(data.get('private') == 'true')
+
             updated_facility = facilities.update_one({'_id': ObjectId(f_id)},
                 {'$set':
                     {'name': data.get('name'),
+                     'private': data.get('private') == 'true',
+                     'access_code': access_code,
                      'address': {
                           'address_L1': data.get('address_L1'),
                           'address_L2': data.get('address_L2'),
